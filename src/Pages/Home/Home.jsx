b4078cfedfc5339ab6bb91df8502ff1121f151bb
gizish <gizawugig@gmail.com>
2026-06-24 12:23:48 +0300

    Banner

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import "./Home.css";
+import Banner from "../../components/Banner/Banner";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Banner />
       <Footer />
     </div>
   );
